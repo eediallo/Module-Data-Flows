c8627987f4731560253daf05dd85c53e6f0084f7
eediallo <elhhabdouldiallo@gmail.com>
2024-11-17 13:33:15 +0000

    clone film card

--- prep/Sprint-1/script.js
+++ prep/Sprint-1/script.js
@@ -7,20 +7,10 @@ const film =   {
   }
 
 
-// sub goal: create a film card
-// 1. create a film card (section)
-// 2. append it to the dom
+// sub goal: refactor to use template
 
 
-// next: render title
-// create h1
-// set title content to title of film
-const filmCard = document.createElement('section')
-const body = document.body
-const title = document.createElement('h1')
-const director = document.createElement('p')
-title.textContent = film.title
-director.textContent = film.director
-filmCard.appendChild(title)
-filmCard.appendChild(director)
-body.appendChild(filmCard)
+const filmCard = document.getElementById('film-card-template').content.cloneNode(true)
+
+
+console.log(filmCard)
