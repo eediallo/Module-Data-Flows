d09cd543c2585d1295f1c1437eeb4971ad896a45
eediallo <elhhabdouldiallo@gmail.com>
2024-11-17 14:15:31 +0000

    iterate through films and creates a film card

--- prep/Sprint-1/script.js
+++ prep/Sprint-1/script.js
@@ -1,18 +1,19 @@
-const film1 =   {
+const films =  [ 
+  {
     title: "Killing of Flower Moon",
     director: "Martin Scorsese",
     times: ["15:35"],
     certificate: "15",
     duration: 112,
-  }
-
-  const film2 =  {
+  }, 
+  {
     title: "Typist Artist Pirate King",
     directory: "Carol Morley",
     times: ["15:00", "20:00"],
     certificate: "12A",
     duration: 108,
   }
+]
 
 // sub goal: refactor to use template
 
@@ -25,4 +26,7 @@ function createFilmCard(film){
   return filmCard
 }
 
-document.body.append(createFilmCard(film1), createFilmCard(film2))
+for(let film of films){
+  const card = createFilmCard(film)
+  document.body.append(card)
+}
