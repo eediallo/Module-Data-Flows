090bf24e718de8397f511de3ccd9ad6b257d4c75
eediallo <elhhabdouldiallo@gmail.com>
2024-11-17 13:15:11 +0000

    renders title on the page

--- prep/Sprint-1/script.js
+++ prep/Sprint-1/script.js
@@ -5,3 +5,19 @@ const film =   {
     certificate: "15",
     duration: 112,
   }
+
+
+// sub goal: create a film card
+// 1. create a film card (section)
+// 2. append it to the dom
+
+
+// next: render title
+// create h1
+// set title content to title of film
+const filmCard = document.createElement('section')
+const body = document.body
+const title = document.createElement('h1')
+title.textContent = film.title
+filmCard.appendChild(title)
+body.appendChild(filmCard)
