abb5a149160f3a2bf75453cede81f8223b3bf269
eediallo <elhhabdouldiallo@gmail.com>
2024-12-02 12:40:47 +0000

    remove comment

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -4,8 +4,7 @@ const personOne = {
   favouriteFood: "Spinach",
 };
 
-// Update the parameter to this function to make it work.
-// Don't change anything else.
+
 function introduceYourself({name, age, favouriteFood} = personOne) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
