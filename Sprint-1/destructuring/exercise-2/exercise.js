16d59ad04391d585a60ca4e92ee071334c5bbec4
eediallo <elhhabdouldiallo@gmail.com>
2024-12-02 13:12:00 +0000

    displays peoples names using object destructuring

--- Sprint-1/destructuring/exercise-2/exercise.js
+++ Sprint-1/destructuring/exercise-2/exercise.js
@@ -70,3 +70,10 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+
+
+  for (const { firstName, lastName, house } of hogwarts) {
+    if (house === "Gryffindor") {
+      console.log(firstName, lastName);
+    }
+  }
