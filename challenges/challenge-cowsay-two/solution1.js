d760b0b34c2b5c48dca21000a814410219dc6d54
eediallo <elhhabdouldiallo@gmail.com>
2024-12-08 12:05:25 +0000

    cow-say --> remove and add comments

--- challenges/challenge-cowsay-two/solution1.js
+++ challenges/challenge-cowsay-two/solution1.js
@@ -5,23 +5,16 @@ const { argv } = require("node:process");
 // https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line
 // =================
 
-// 1. Accept arguments
-
-// how will you accept arguments?
-
 //require at least one additional argument
 if (argv.length === 2) {
   console.log("Should accept at least one argument");
   process.exit(1);
 }
 
-// 2. Make supplies for our speech bubble
-
 let topLine = "_";
 let bottomLine = "-";
 let saying = argv[2];
 
-// 3. Make a cow that takes a string
 const cow = `
         \\\   ^__^
          \\\  (oo)\\\_______
@@ -30,6 +23,7 @@ const cow = `
                 ||     ||
 `;
 
+// write top/bottom line based on provided CI argument
 function writeLine(line, saying) {
   process.stdout.write(" ");
   for (let i = 0; i <= saying.length + 1; i++) {
@@ -47,7 +41,3 @@ function cowsay(saying) {
 
 cowsay(saying);
 console.log(cow);
-
-//4. Pipe argument into cowsay function and return a cow
-
-// how will you log this to the console?
