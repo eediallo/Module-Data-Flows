8e1d5a5092a1ae51ca6cb523543db8793fafbf15
eediallo <elhhabdouldiallo@gmail.com>
2024-12-08 13:12:59 +0000

    makes buddle speech

--- challenges/challenge-cowsay-two/solution2.js
+++ challenges/challenge-cowsay-two/solution2.js
@@ -1,4 +1,5 @@
 const readline = require("node:readline");
+const process = require("node:process");
 
 // =================
 // Stripped down cowsayer CLI,
@@ -10,12 +11,39 @@ const readline = require("node:readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 // 2. Make supplies for our speech bubble
+let topLine = "";
+let bottomLine = "";
+let saying = "";
+
+rl.question("Please Enter the top Line: ", (top) => {
+  rl.question("Please Enter the bottom line: ", (bottom) => {
+    rl.question("Please Enter cow saying: ", (say) => {
+      topLine += top;
+      bottomLine += bottom;
+      saying += say;
+      cowsay(saying)
+      console.log()
+      rl.close();
+    });
+  });
+});
 
 // 3. Make a cow that takes a string
 
-const cow = (saying) => {
-  // how did you make the cow before?
-};
+function writeLine(line, saying) {
+  process.stdout.write(" ");
+  for (let i = 0; i <= saying.length + 1; i++) {
+    process.stdout.write(line);
+  }
+}
+
+function cowsay(saying) {
+  writeLine(topLine, saying);
+  console.log();
+  process.stdout.write(`< ${saying} >`);
+  console.log();
+  writeLine(bottomLine, saying);
+}
 
 // 4. Use readline to get a string from the terminal
 // (with a prompt so it's clearer what we want)
