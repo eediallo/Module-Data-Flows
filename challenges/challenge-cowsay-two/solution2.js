d1ef1fd95222d1915234cc7a0cd8cc6ba20f4b45
eediallo <elhhabdouldiallo@gmail.com>
2024-12-08 12:30:49 +0000

    cow-say 2 --> makes command line interface

--- challenges/challenge-cowsay-two/solution2.js
+++ challenges/challenge-cowsay-two/solution2.js
@@ -1,18 +1,21 @@
+const readline = require("node:readline");
+
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowsayer CLI,
 // no libraries or arguments
 // https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
 // =================
 
 // 1. Make  a command line interface.
+const rl = readline.createInterface(process.stdin, process.stdout);
 
 // 2. Make supplies for our speech bubble
 
 // 3. Make a cow that takes a string
 
 const cow = (saying) => {
-    // how did you make the cow before?
-}
+  // how did you make the cow before?
+};
 
-// 4. Use readline to get a string from the terminal 
+// 4. Use readline to get a string from the terminal
 // (with a prompt so it's clearer what we want)
