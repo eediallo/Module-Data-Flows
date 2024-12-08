496ac91807caf90619f00b2ce9ba34bc0c81f274
eediallo <elhhabdouldiallo@gmail.com>
2024-12-08 11:31:27 +0000

    cow-say --> create function writeLine to make top and bottom lines

--- challenges/challenge-cowsay-two/solution1.js
+++ challenges/challenge-cowsay-two/solution1.js
@@ -1,6 +1,6 @@
-const {argv} = require('node:process')
+const { argv } = require("node:process");
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowsayer CLI,
 // no libraries
 // https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line
 // =================
@@ -10,16 +10,16 @@ const {argv} = require('node:process')
 // how will you accept arguments?
 
 //require at least one additional argument
-if(argv.length === 2){
-    console.log('Should accept at least one argument')
-    process.exit(1)
+if (argv.length === 2) {
+  console.log("Should accept at least one argument");
+  process.exit(1);
 }
 
 // 2. Make supplies for our speech bubble
 
-let topLine = '_';
-let bottomLine = '-';
-let saying = '';
+let topLine = "_";
+let bottomLine = "-";
+let saying = "";
 
 // 3. Make a cow that takes a string
 const cow = `
@@ -28,15 +28,19 @@ const cow = `
             (__)\\\       )\\/\\
                 ||----w |
                 ||     ||
-`
+`;
+
+function writeLine(line, saying) {
+  process.stdout.write(" ");
+  for (let i = 0; i <= saying.length + 1; i++) {
+    process.stdout.write(line);
+  }
+}
 
 function cowsay(saying) {
-// how will you make the speech bubble contain the text?
-
-// where will the cow picture go?
-
-// how will you account for the parameter being empty?
-
+  // how will you make the speech bubble contain the text?
+  // where will the cow picture go?
+  // how will you account for the parameter being empty?
 }
 
 //4. Pipe argument into cowsay function and return a cow
