4e353d4ce3ab6e27b91bdbbcdd746766bd7c2093
eediallo <elhhabdouldiallo@gmail.com>
2024-12-11 18:09:17 +0000

    calculator --> write test to check data type of param

--- challenges/unit-testing/katas-tdd/calculator/calculator.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.js
@@ -1,6 +1,7 @@
-function sum(str) {
-  return "";
+function add(numbers) {
+  if (typeof numbers !== "string") {
+    throw new Error("Parameter must be a string.");
+  }
 }
 
-
-module.exports = {sum}
+module.exports = add;
--- challenges/unit-testing/katas-tdd/calculator/calculator.test.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.test.js
@@ -1,9 +1,8 @@
-const sum = require("./calculator.js");
+const add = require("./calculator.js");
 
 test("throw an error if num is not a string", () => {
-  const target = "Parameter must be a string";
+  const target = "Parameter must be a string.";
   expect(() => {
-    const str = 9;
-    sum(str);
+    add(9)
   }).toThrow(target);
 });
