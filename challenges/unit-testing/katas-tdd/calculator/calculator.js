2a67e8ca8a105c6efb966bedf61621d71a7c39be
eediallo <elhhabdouldiallo@gmail.com>
2024-12-11 18:14:16 +0000

    calculator --> return 0 if param is empty

--- challenges/unit-testing/katas-tdd/calculator/calculator.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.js
@@ -1,6 +1,11 @@
 function add(numbers) {
+  let sum = 0;
   if (typeof numbers !== "string") {
     throw new Error("Parameter must be a string.");
+  }
+
+  if (numbers === "") {
+    return 0
   }
 }
 
--- challenges/unit-testing/katas-tdd/calculator/calculator.test.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.test.js
@@ -3,6 +3,13 @@ const add = require("./calculator.js");
 test("throw an error if num is not a string", () => {
   const target = "Parameter must be a string.";
   expect(() => {
-    add(9)
+    add(9);
   }).toThrow(target);
+});
+
+test("return 0 if param is empty", () => {
+  const str = "";
+  const currentInput = add(str);
+  const targetOutput = 0;
+  expect(currentInput).toEqual(targetOutput);
 });
