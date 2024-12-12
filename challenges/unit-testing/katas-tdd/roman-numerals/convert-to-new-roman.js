b724798384df28efa68c304a502939282e2412ad
eediallo <elhhabdouldiallo@gmail.com>
2024-12-12 20:05:15 +0000

    roman-numerals --> returns I if passed 1 as an argument

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
@@ -1,3 +1,7 @@
-function convertToNewRoman(n) {}
+function convertToNewRoman(n) {
+  if (n === 1) {
+    return "I";
+  }
+}
 
 module.exports = convertToNewRoman;
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
@@ -1,7 +1,7 @@
 let convertToNewRoman = require("./convert-to-new-roman");
 
 test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  // Act
-  // Assert
+  const currentInput = convertToNewRoman(1);
+  const targetOutput = "I";
+  expect(currentInput).toBe(targetOutput);
 });
