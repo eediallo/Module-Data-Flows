d9ee68d658c53aa28c603b73477164468f3bb76e
eediallo <elhhabdouldiallo@gmail.com>
2024-12-12 20:13:58 +0000

    roman-numerals --> returns II if passed 2 as an argument

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
@@ -1,7 +1,14 @@
 function convertToNewRoman(n) {
+  let numeral = "";
   if (n === 1) {
     return "I";
   }
+
+  for (let i = 0; i < n; i++) {
+    numeral += "I";
+  }
+
+  return numeral
 }
 
 module.exports = convertToNewRoman;
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
@@ -5,3 +5,9 @@ test("returns I if passed 1 as an argument", function () {
   const targetOutput = "I";
   expect(currentInput).toBe(targetOutput);
 });
+
+test("returns II if passed 2 as an argument", function () {
+  const currentInput = convertToNewRoman(2);
+  const targetOutput = "II";
+  expect(currentInput).toBe(targetOutput);
+});
