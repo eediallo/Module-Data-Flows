a2c6feeb85f0880f9c3560c4d2ffcb5e693056b5
eediallo <elhhabdouldiallo@gmail.com>
2024-12-12 19:34:43 +0000

    password-verifier --> rejects password without at least one uppercase letter

--- challenges/unit-testing/katas-tdd/password-verifier/passwordVerifier.js
+++ challenges/unit-testing/katas-tdd/password-verifier/passwordVerifier.js
@@ -6,6 +6,17 @@ function verifier(password) {
   if (password.length < 8) {
     return "Password rejected";
   }
+
+  if (!includesUpperCaseLetter(password)) {
+    return "Password rejected";
+  }
+
+  return 'Password Accepted'
 }
+
+function includesUpperCaseLetter(str) {
+  return Boolean(str.match(/[A-Z]/g));
+}
+
 
 module.exports = verifier;
--- challenges/unit-testing/katas-tdd/password-verifier/passwordVerifier.test.js
+++ challenges/unit-testing/katas-tdd/password-verifier/passwordVerifier.test.js
@@ -2,13 +2,19 @@ const verify = require("./passwordVerifier.js");
 
 describe("password verifier", () => {
   test("rejects password if its length is less than 8", () => {
-    const currentInput = verify("938u");
+    const currentInput = verify("eu4u");
     const targetOutput = "Password rejected";
     expect(currentInput).toBe(targetOutput);
   });
 
   test("rejects a null password", () => {
     const currentInput = verify(null);
+    const targetOutput = "Password rejected";
+    expect(currentInput).toBe(targetOutput);
+  });
+
+  test("rejected password without a least one uppercase letter", () => {
+    const currentInput = verify("9938hello");
     const targetOutput = "Password rejected";
     expect(currentInput).toBe(targetOutput);
   });
