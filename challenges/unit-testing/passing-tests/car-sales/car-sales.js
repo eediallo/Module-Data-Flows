c4bfb7db8ccd0fb43e500cc58a283128e988b6b4
eediallo <elhhabdouldiallo@gmail.com>
2024-12-14 23:19:57 +0000

    car-sales --> makes sales test pass by fixing sales function

--- challenges/unit-testing/passing-tests/car-sales/car-sales.js
+++ challenges/unit-testing/passing-tests/car-sales/car-sales.js
@@ -1,3 +1,25 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  const totals = {
+    Ford: 0,
+    Honda: 0,
+    "Land Rover": 0,
+    Toyota: 0,
+  };
+
+  for (let carDetails of carsSold) {
+    console.log(carDetails.make);
+    if (carDetails.make === "Ford") {
+      totals.Ford += carDetails.price;
+    } else if (carDetails.make === "Honda") {
+      totals.Honda = carDetails.price;
+    } else if (carDetails.make === "Land Rover") {
+      totals["Land Rover"] += carDetails.price;
+    } else {
+      totals.Toyota += carDetails.price;
+    }
+  }
+
+  return totals;
+}
 
 module.exports = sales;
