6d160c2736179274e4c332d04f3aab0aeaf3f68c
eediallo <elhhabdouldiallo@gmail.com>
2024-12-14 23:52:54 +0000

    get average-> calculate average of numbers

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,14 +2,13 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function compare(a, b) {
-  return a - b;
-}
 function average(numbers) {
   const onlyNumbers = numbers.filter((item) => typeof item === "number");
-  const sortedNumbers = onlyNumbers.sort(compare);
-  console.log(sortedNumbers);
-}
+  let sum = 0;
+  onlyNumbers.forEach((num) => {
+    sum += num;
+  });
 
-average([4, 100, "-", 8, 11, "hello", "57", 0, 2, 10]);
+  return sum / onlyNumbers.length;
+}
 module.exports = average;
