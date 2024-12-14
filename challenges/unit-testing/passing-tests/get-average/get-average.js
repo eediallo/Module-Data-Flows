5c90ad2188987c2a46baad46d9d18a32c354a51f
eediallo <elhhabdouldiallo@gmail.com>
2024-12-14 23:42:33 +0000

    get average-> filter only numbers

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,10 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  const onlyNumbers = numbers.filter((item) => typeof item === "number");
+  console.log(onlyNumbers);
+}
 
+average([4, "-", 8, 11, "hello", "57", 0, 2]);
 module.exports = average;
