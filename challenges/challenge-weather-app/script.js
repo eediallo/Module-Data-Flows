38eb886d971350ecf0d0f4649dea803e3d39720e
eediallo <elhhabdouldiallo@gmail.com>
2024-12-19 13:59:40 +0000

    update state and ui classes

--- challenges/challenge-weather-app/script.js
+++ challenges/challenge-weather-app/script.js
@@ -32,6 +32,24 @@ class State {
     return this.hasDataLoadSuccessfully ? State.successMsg : State.unsuccessMsg;
   }
 
+  async fetchWeatherData() {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${State.weatherAPIKey}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.error(`Response status: ${response.status}`);
+    }
+    this.weatherData = await response.json();
+  }
+
+  async fetchPhotos() {
+    const url = `https://api.unsplash.com/search/photos?query=${this.weatherData.weather[0].description}&client_id=${State.unsplashAccessKey}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.error(`Response status: ${response.status}`);
+    }
+    return await response.json();
+  }
+
   styleFeedbackMsg(element) {
     element.style.textAlign = "center";
     element.style.fontSize = "20px";
@@ -59,35 +77,57 @@ class ActiveThumbnail {
   }
 }
 
+class UI {
+  constructor() {
+    this.thumbCards = [];
+  }
+
+  createThumbCard(photo) {
+    const thumbCard = document.createElement("section");
+    const aEl = document.createElement("a");
+    aEl.setAttribute("href", photo.urls.full);
+    aEl.classList.add("id", "to-main-img");
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.thumb);
+    img.setAttribute("alt", photo.alt_description);
+    aEl.append(img);
+    thumbCard.append(aEl);
+    thumbCard.classList.add("thumb-card");
+    // Add click event listener to load full image
+    aEl.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      aThumbnail.handleActiveThumbnail(thumbCard);
+
+      loadMainImage(photo.urls.full, photo.alt_description);
+      //display user name et link to portfolio
+      creditUser.textContent = `${photo.user.first_name} ${photo.user.last_name}`;
+      creditUser.setAttribute("href", photo.user.links.portfolio);
+    });
+
+    return thumbCard;
+  }
+
+  updateUI(state) {
+    this.thumbCards = state.photos.results.map(this.createThumbCard);
+    thumbs.append(...this.thumbCards);
+    conditions.textContent = state.weatherData.weather[0].description;
+  }
+}
+
 const state = new State(false, true);
-
 const aThumbnail = new ActiveThumbnail(activeThumbnail, "data-active");
+const ui = new UI();
 
-// set city on clicked before fetching data
-searchBtn.addEventListener("click", (event) => {
+searchBtn.addEventListener("click", async (event) => {
   event.preventDefault();
   state.city = searchTerm.value;
-  fetchData();
+  await state.fetchWeatherData();
+  const photos = await state.fetchPhotos();
+  state.photos = photos;
+  ui.updateUI(state);
 });
 
-async function getWeatherData() {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${state.city}&appid=${State.weatherAPIKey}`;
-  const response = await fetch(url);
-  if (!response.ok) {
-    console.error(`Response status: ${response.status}`);
-  }
-  return response.json();
-}
-
-async function getPhotos() {
-  const url = `https://api.unsplash.com/search/photos?query=${state.weatherData.weather[0].description}&client_id=${State.unsplashAccessKey}`;
-  const response = await fetch(url);
-
-  if (!response.ok) {
-    console.error(`Response status: ${response.status}`);
-  }
-  return await response.json();
-}
 
 function msgToUser(element, isError) {
   if (!isError) {
@@ -99,57 +139,6 @@ function msgToUser(element, isError) {
   }
 }
 
-async function fetchData() {
-  msgToUser(loadingMsgEl, false);
-  state.isFetching = true;
-  try {
-    const weatherData = await getWeatherData();
-    state.weatherData = weatherData;
-
-    const photos = await getPhotos();
-    state.photos = photos;
-
-    updateUI();
-    loadingMsgEl.remove(); // Remove loading message only if there is no error
-  } catch (error) {
-    msgToUser(loadingMsgEl, true);
-    console.error(error);
-  } finally {
-    state.isFetching = false;
-  }
-}
-
-function createThumbCard(photo) {
-  const thumbCard = document.createElement("section");
-  const aEl = document.createElement("a");
-  aEl.setAttribute("href", photo.urls.full);
-  aEl.classList.add("id", "to-main-img");
-  const img = document.createElement("img");
-  img.setAttribute("src", photo.urls.thumb);
-  img.setAttribute("alt", photo.alt_description);
-  aEl.append(img);
-  thumbCard.append(aEl);
-  thumbCard.classList.add("thumb-card");
-  // Add click event listener to load full image
-  aEl.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    aThumbnail.handleActiveThumbnail(thumbCard);
-
-    loadMainImage(photo.urls.full, photo.alt_description);
-    //display user name et link to portfolio
-    creditUser.textContent = `${photo.user.first_name} ${photo.user.last_name}`;
-    creditUser.setAttribute("href", photo.user.links.portfolio);
-  });
-
-  return thumbCard;
-}
-
-function updateUI() {
-  const thumbCards = state.photos.results.map(createThumbCard);
-  thumbs.append(...thumbCards);
-  conditions.textContent = state.weatherData.weather[0].description;
-}
 function loadMainImage(url, alt) {
   const mainImg = document.querySelector("#main-img");
   mainImg.setAttribute("src", url);
