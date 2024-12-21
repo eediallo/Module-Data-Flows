5742851c7963ecfb1160d9cad77dd66f16c54841
eediallo <elhhabdouldiallo@gmail.com>
2024-12-21 22:11:29 +0000

    movees Photos class to its own file

---
+++ challenges/challenge-weather-app/photos.js
@@ -0,0 +1,19 @@
+import { Weather } from "./weather.js";
+
+class Photos {
+  constructor(weatherData) {
+    this.weatherData = weatherData;
+    this.photos = {};
+  }
+
+  static unsplashAccessKey = config.unsplash_access_key;
+
+  async fetchPhotos() {
+    const url = `https://api.unsplash.com/search/photos?query=${this.weatherData.weather[0].description}&client_id=${Photos.unsplashAccessKey}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.error(`Response status: ${response.status}`);
+    }
+    this.photos = await response.json();
+  }
+}
