cfa7118b90f72f08662d8a3eb2055809b8fbe036
eediallo <elhhabdouldiallo@gmail.com>
2024-12-22 00:52:57 +0000

    adds a button to remove the dog gallery

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -4,11 +4,23 @@ const gallery = document.createElement("div");
 gallery.classList.add("photo-gallery");
 const displayDogBtn = document.createElement("button");
 displayDogBtn.textContent = "Click to display random dog";
+const removeDogGalleryBtn = document.createElement("button");
+removeDogGalleryBtn.textContent = "Remove Dog Gallery";
 
-document.body.append(gallery, displayDogBtn, ul, loadMsgEl);
+document.body.append(
+  gallery,
+  displayDogBtn,
+  ul,
+  loadMsgEl,
+  removeDogGalleryBtn
+);
 
 // Event listener
 displayDogBtn.addEventListener(
   "click",
   dogGallery.displayDogImage.bind(dogGallery)
 );
+
+removeDogGalleryBtn.addEventListener("click", () => {
+  document.querySelector("ul").innerHTML = "";
+});
