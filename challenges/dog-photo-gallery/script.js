e2cc603dab07ae1f32108cfe6e8a06f80db4e834
eediallo <elhhabdouldiallo@gmail.com>
2024-12-22 01:03:00 +0000

    update event lister to create remove button only when it does not exist

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -4,23 +4,26 @@ const gallery = document.createElement("div");
 gallery.classList.add("photo-gallery");
 const displayDogBtn = document.createElement("button");
 displayDogBtn.textContent = "Click to display random dog";
-const removeDogGalleryBtn = document.createElement("button");
-removeDogGalleryBtn.textContent = "Remove Dog Gallery";
 
 document.body.append(
   gallery,
   displayDogBtn,
   ul,
-  loadMsgEl,
-  removeDogGalleryBtn
+  loadMsgEl
+  // removeDogGalleryBtn
 );
 
 // Event listener
-displayDogBtn.addEventListener(
-  "click",
-  dogGallery.displayDogImage.bind(dogGallery)
-);
-
-removeDogGalleryBtn.addEventListener("click", () => {
-  document.querySelector("ul").innerHTML = "";
+displayDogBtn.addEventListener("click", () => {
+  dogGallery.displayDogImage.call(dogGallery);
+  
+  if (!document.querySelector(".remove-dog-gallery-btn")) {
+    const removeDogGalleryBtn = document.createElement("button");
+    removeDogGalleryBtn.textContent = "Remove Dog Gallery";
+    removeDogGalleryBtn.classList.add("remove-dog-gallery-btn");
+    document.body.append(removeDogGalleryBtn);
+    removeDogGalleryBtn.addEventListener("click", () => {
+      document.querySelector("ul").innerHTML = "";
+    });
+  }
 });
